d3ea88466b87b9412035e2e6bd172d80411708b4
Vikram Sharma <vikramsharma1720@outlook.com>
2022-07-13 16:01:43 +0545

    React crud app

---
+++ src/data/index.js
@@ -0,0 +1,45 @@
+const employeesData = [
+    {
+      id: 1,
+      firstName: 'Susan',
+      lastName: 'shah',
+      email: 'susan@example.com',
+      salary: '95000',
+      date: '2019-04-11',
+    },
+    {
+      id: 2,
+      firstName: 'pratik',
+      lastName: 'Dhakal',
+      email: 'pdhakal@example.com',
+      salary: '80000',
+      date: '2019-04-17',
+    },
+    {
+      id: 3,
+      firstName: 'nisha',
+      lastName: 'lama',
+      email: 'nisha@example.com',
+      salary: '79000',
+      date: '2019-05-01',
+    },
+    {
+      id: 4,
+      firstName: 'Kent',
+      lastName: 'RO',
+      email: 'kentRO@example.com',
+      salary: '56000',
+      date: '2019-05-03',
+    },
+    {
+      id: 5,
+      firstName: 'jatayu',
+      lastName: 'sen',
+      email: 'sen12o@example.com',
+      salary: '65000',
+      date: '2019-06-13',
+    }
+  ];
+  
+  export { employeesData };
+  
